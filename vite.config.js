a2cbc6576b4f2562c04daffbb85351dce301c9a3
vikaschandra38 <vikaschandra38@gmail.com>
2026-02-11 23:36:19 +0530

    removed basepath

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({  base: '/my-portfolio/',  plugins: [react()],
-})
+export default defineConfig({ plugins: [react()] });
